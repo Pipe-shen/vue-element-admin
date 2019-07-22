a331ce08b0bc1152370f9b18b35724c8a819383c
pipe <yys320carter@163.com>
2019-07-22 14:07:36 +0800

    清理 dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -3,50 +3,17 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-/* Layout */
 import Layout from '@/layout'
 
 /* Router Modules */
+// 所有系统的通用路由
 import constantRoutes from './modules/constant'
-
-/* import iconRouter from './modules/icon'
-import tabRouter from './modules/tab'
-import excelRouter from './modules/excel'
-import zipRouter from './modules/zip'
-import pdfRouter from './modules/pdf'
-import themeRouter from './modules/theme'
-import externalRouter from './modules/external'
-import clipboardRouter from './modules/clipboard'
-import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
-import nestedRouter from './modules/nested' */
+// CMS 管理
 import cmsRouter from './modules/cms'
 
 /**
- * Note: sub-menu only appear when route children.length >= 1
- * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
- *
- * hidden: true                   if set true, item will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu
- *                                if not set alwaysShow, when item has more than one children route,
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
- * name:'router-name'             the name is used by <keep-alive> (must set!!!)
- * meta : {
-    roles: ['admin','editor']    control the page roles (you can set multiple roles)
-    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
-    icon: 'svg-name'             the icon show in the sidebar
-    noCache: true                if set true, the page will no be cached(default is false)
-    affix: true                  if set true, the tag will affix in the tags-view
-    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
-  }
- */
-
-/**
- * asyncRoutes
- * the routes that need to be dynamically loaded based on user roles
+ * 异步路由
+ * 根据用户角色权限来动态加载
  */
 export const asyncRoutes = [
   cmsRouter,
@@ -129,67 +96,6 @@ export const asyncRoutes = [
     ]
   },
 
-  /** when your routing map is too long, you can split it into small modules **/
-  /* {
-    path: '/demo',
-    component: Layout,
-    redirect: '/demo/icons',
-    alwaysShow: true, // will always show the root menu
-    name: 'Demo',
-    meta: {
-      title: 'Demo',
-      icon: 'lock',
-      roles: ['admin', 'editor'] // you can set roles in root nav
-    },
-    children: [
-      iconRouter
-    ]
-  },
-  iconRouter,
-  tabRouter,
-  excelRouter,
-  zipRouter,
-  pdfRouter,
-  themeRouter,
-  componentsRouter,
-  chartsRouter,
-  nestedRouter,
-  tableRouter,
-  clipboardRouter,
-  externalRouter, */
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/list',
-    name: 'Example',
-    meta: {
-      title: 'Example',
-      icon: 'example'
-    },
-    children: [
-      {
-        path: 'create',
-        component: () => import('@/views/example/create'),
-        name: 'CreateArticle',
-        meta: { title: 'Create Article', icon: 'edit' }
-      },
-      {
-        path: 'edit/:id(\\d+)',
-        component: () => import('@/views/example/edit'),
-        name: 'EditArticle',
-        meta: { title: 'Edit Article', noCache: true, activeMenu: '/example/list' },
-        hidden: true
-      },
-      {
-        path: 'list',
-        component: () => import('@/views/example/list'),
-        name: 'ArticleList',
-        meta: { title: 'Article List', icon: 'list' }
-      }
-    ]
-  },
-
-  // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
 ]
 
@@ -201,7 +107,6 @@ const createRouter = () => new Router({
 
 const router = createRouter()
 
-// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
   const newRouter = createRouter()
   router.matcher = newRouter.matcher // reset router
